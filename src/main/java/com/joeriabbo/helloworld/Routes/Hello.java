48589bf245d87e8aad4d10ae635f6335a0bbcb16
Joeri Abbo <joeriabbo@hotmail.com>
2022-07-17 13:06:22 +0200

    Cleanup

--- src/main/java/com/joeriabbo/helloworld/Routes/Hello.java
+++ src/main/java/com/joeriabbo/helloworld/Routes/Hello.java
@@ -1,11 +1,9 @@
 package com.joeriabbo.helloworld.Routes;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Configuration
 public class Hello {
-
     public String response(String name) {
         return String.format("Hello %s!", name);
     }
--- src/main/java/com/joeriabbo/helloworld/Routes/Root.java
+++ src/main/java/com/joeriabbo/helloworld/Routes/Root.java
@@ -1,7 +1,5 @@
 package com.joeriabbo.helloworld.Routes;
 
-import org.springframework.web.bind.annotation.RequestParam;
-
 public class Root {
     public String response() {
         return "Hello!";
